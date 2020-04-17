6f4e3ec71edb4913c3367661ad4da8cfa9d95efb
renato119 <castillo.renato.90@gmail.com>
2020-04-17 14:24:33 -0400

    Merge branch 'Ampèremètre' of git.step.polymtl.ca:gbm2100-H20/g1/e/pstat-psoc into Ampèremètre
    # Conflicts:
    #	Pstat.cydsn/main.c

--- Pstat.cydsn/main.c
+++ Pstat.cydsn/main.c
@@ -1,49 +1,82 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
+ * Copyright GBM2100 Équipe 1E, 2020
  * All Rights Reserved
  * UNPUBLISHED, LICENSED SOFTWARE.
  *
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
+ * WHICH IS THE PROPERTY OF Équipe 1E.
+ * Renato Castillo          1962797
+ * Karl-Philippe Beaudet    1958657
+ * Richema Métallus         1953911
+ * Tasnim Ahmed             1958545
  * ========================================
 */
+
 #include "project.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdio.h>
 
-
-int main(void)
+///////////////////////////// FUNCTIONS BODY /////////////////////////////
+void UART_initialisation() 
 {
+    UART_Start();
+    UART_PutString("- Bienvenue au multimetre de l'equipe 1e - \n\r ");
+    CyDelay(2000);
+    UART_PutString("- Veuillez choisir un mode - \n \r");
+    CyDelay(4000);
+   
+}
+
+void mode_Amp () // Configuration de l'amperemetre
+{
+    UART_PutString("- Mode Amperemetre -\n\r");
     int32 adcResult;
     float adcVolt;
     float adcAmp;
     float R_2;
     char result_Amp[20];
-    
+    ADC_IsEndConversion(ADC_RETURN_STATUS);
+    adcResult=ADC_GetResult32();
+    adcVolt=ADC_CountsTo_mVolts(adcResult);
+    adcAmp = adcVolt/R_2;
+    sprintf(result_Amp,"%+1.3f A",adcAmp);
+    UART_PutString(result_Amp);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// MAIN LOOP /////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+int main(void)
+{
     CyGlobalIntEnable; /* Enable global interrupts. */
     FreeRTOS_Start();
-    UART_Start();
     ADC_Start();
-    UART_PutString("- Mode Amperemetre -\n\r");
     ADC_StartConvert();
-    
+    char8 input;
     
 //    xTaskCreate(...);
     
     vTaskStartScheduler();
     for(;;) 
     {
-        ADC_IsEndConversion(ADC_RETURN_STATUS);
-        adcResult=ADC_GetResult32();
-        adcVolt=ADC_CountsTo_mVolts(adcResult);
-        adcAmp = adcVolt/R_2;
-        sprintf(result_Amp,"%+1.3f A",adcAmp);
-        UART_PutString(result_Amp);
-        CyDelay(1000);
+       UART_initialisation();
+       input=UART_GetChar(); // Permet de savoir le mode choisi
+       if (input=='1')
+        {
+            mode_Amp();
+        }
+       else
+        {
+            UART_PutString("-No mode selected-\n\r");
+        }
+       CyDelay(3000);
     }
 }
+ ///////////////////=========== main loop Ends Here ===============/////////////////////////
 
 /* [] END OF FILE */
