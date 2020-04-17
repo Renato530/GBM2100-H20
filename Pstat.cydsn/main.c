7060d5942156028acd14199edd1e6ad3cacc9cac
renato119 <castillo.renato.90@gmail.com>
2020-04-17 14:04:30 -0400

    Creation Ampèremètre Set Up initial de l'ampèremètre (TopDesign) Code Brouillon main.c (sans entête ni commentaires)

--- Pstat.cydsn/main.c
+++ Pstat.cydsn/main.c
@@ -12,17 +12,38 @@
 #include "project.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include <stdio.h>
 
 
 int main(void)
 {
+    int32 adcResult;
+    float adcVolt;
+    float adcAmp;
+    float R_2;
+    char result_Amp[20];
+    
     CyGlobalIntEnable; /* Enable global interrupts. */
     FreeRTOS_Start();
+    UART_Start();
+    ADC_Start();
+    UART_PutString("- Mode Amperemetre -\n\r");
+    ADC_StartConvert();
+    
     
 //    xTaskCreate(...);
     
     vTaskStartScheduler();
-    for(;;) {}
+    for(;;) 
+    {
+        ADC_IsEndConversion(ADC_RETURN_STATUS);
+        adcResult=ADC_GetResult32();
+        adcVolt=ADC_CountsTo_mVolts(adcResult);
+        adcAmp = adcVolt/R_2;
+        sprintf(result_Amp,"%+1.3f A",adcAmp);
+        UART_PutString(result_Amp);
+        CyDelay(1000);
+    }
 }
 
 /* [] END OF FILE */
