06c71e06408f9caae060be4b2941eeabb46cc857
renato119 <castillo.renato.90@gmail.com>
2020-04-19 13:58:43 -0400

    Modifications du main.c

--- Pstat.cydsn/main.c
+++ Pstat.cydsn/main.c
@@ -22,43 +22,74 @@
 
 // Pour l'instant essayer de tester avec des resistances du FreeSoC
 
+///////////////////////////// FUNCTIONS BODY /////////////////////////////
 
-int main(void)
+////////////////////////
+// Cette fonction permet d'initialiser le UART et selectioner un mode du multimetre
+////////////////////////
+void UART_initialisation() 
 {
-    // Variables générales
+    UART_Start();
+    // Affichage du MENU
+    UART_PutString("== MULTIMETRE == \n \r");
+    UART_PutString("  0 - Mode Voltmetre \n \r");
+    UART_PutString("  1 - Mode Amperemetre \n \r");
+    UART_PutString("  2 - Mode Ohmmetre \n \r");
+    UART_PutString("- Veuillez choisir un mode - \n \r");
+    CyDelay(4000);
+}
+
+////////////////////////
+// Cette fonction permet l'utilisation du voltmetre 
+////////////////////////
+
+void mode_Ohmetre()
+{
     int courant = 0;
     int resistance = 0;
     int voltage = 0;
     char val_resistance[10];
+    uint DAC_valeur = 0xff;
+    int compte=4095;
+    
+    DAC_Start();
+    SAR_ADC_Start();
+    DAC_valeur= 0xff;
+    DAC_SetValue(DAC_valeur);
+    SAR_ADC_StartConvert();
+    while ((DAC_valeur > 0) && (compte >=4000))
+    {
+        DAC_valeur=DAC_valeur - 0x01;
+        DAC_SetValue(DAC_valeur);
+        CyDelay(10);
+    }
+        if (SAR_ADC_IsEndConversion(SAR_ADC_WAIT_FOR_RESULT)!=0)
+        {
+            compte= SAR_ADC_GetResult16(0);
+        }
+    
+    
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// MAIN LOOP /////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+
+int main(void)
+{
+    // Variables générales
     
     CyGlobalIntEnable; /* Enable global interrupts. */
     FreeRTOS_Start();
-    DAC_Start();
-    SAR_ADC_Start();
-   
-    SAR_ADC_StartConvert();
-    UART_PutString("Ohmetre \r\r");
     
-    vTaskStartScheduler();
     for(;;) 
     {
-        courant = 200; // uA 
-        if (SAR_ADC_IsEndConversion(SAR_ADC_RETURN_STATUS)) {} // TODO : Essayer de verifier si la conversion est fini pour faire le calcul
-        voltage= SAR_ADC_CountsTo_uVolts(SAR_ADC_GetResult16()); // uV
-        resistance = voltage/courant;
-        sprintf(val_resistance,"%d",resistance);
-        if (resistance!=0)
-        {
-            UART_PutString(val_resistance);
-            UART_PutString("\n");
-            UART_PutString("Ohms");
-        }
-        else 
-        {
-            UART_PutString("\n\r");
-            UART_PutString("Veuillez inserer la resistance");
-        }
+       
     }
-}
+} ///////////////////=========== main loop Ends Here ===============/////////////////////////
 
 /* [] END OF FILE */
